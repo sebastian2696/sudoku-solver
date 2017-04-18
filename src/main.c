74650b344ff1124462c781226c75201362229b9a
AaronCoplan <afcoplan@gwu.edu>
2017-04-18 16:38:31 -0400

    Made solving algorithm track more information.  Still need to incorporate error checking.

--- src/main.c
+++ src/main.c
@@ -9,9 +9,9 @@
 
 #define USAGE_INFO "Usage: ./build <sudoku file path>"
 
-static void attempt_row_insertions(int row_num);
-static void attempt_col_insertions(int col_num);
-static void attempt_square_insertions(int square_x, int square_y);
+static int attempt_row_insertions(int row_num);
+static int attempt_col_insertions(int col_num);
+static int attempt_square_insertions(int square_x, int square_y);
 
 int main(int argc, char** argv){
         int row;
@@ -22,6 +22,7 @@ int main(int argc, char** argv){
         int num_unsolved;
         int last_unsolved;
         int exit_code;
+        int changes_made;
         char* file_path;
         struct timeval start_time, end_time;
 
@@ -39,6 +40,7 @@ int main(int argc, char** argv){
         print_sudoku();
 
         num_runs = 0;
+        changes_made = 0;
 
         gettimeofday(&start_time, NULL);
 
@@ -52,20 +54,19 @@ int main(int argc, char** argv){
                 }
 
                 if(num_unsolved == last_unsolved){
-                        printf("Stuck!\n");
                         exit_code = -1;
                         break;
                 }
 
                 for(row = 0; row < 9; ++row){
-                        attempt_row_insertions(row);
+                        changes_made += attempt_row_insertions(row);
                 }
                 for(col = 0; col < 9; ++col){
-                        attempt_col_insertions(col);
+                        changes_made += attempt_col_insertions(col);
                 }
                 for(square_x = 0; square_x < 3; ++square_x){
                         for(square_y = 0; square_y < 3; ++square_y){
-                                attempt_square_insertions(square_x, square_y);
+                                changes_made += attempt_square_insertions(square_x, square_y);
                         }
                 }
 
@@ -74,16 +75,22 @@ int main(int argc, char** argv){
                 last_unsolved = num_unsolved;
         }
 
+
+
         // fix for when it gets stuck
         if(exit_code == -1){
                 // enter some other algorithm, most likely a brute force solution using backtracking
+                printf("Got stuck using intelligent algorithm, entering brute force now...\n");
         }
+
+        printf("Finished!\n");
 
         gettimeofday(&end_time, NULL);
 
         printf("Time in microseconds: %ld\n", ((end_time.tv_sec - start_time.tv_sec)*1000000L +end_time.tv_usec) - start_time.tv_usec);
 
-        printf("Num Runs: %d\n", num_runs);
+        printf("Num Passes: %d\n", num_runs);
+        printf("Number of changes made by intelligent algorithm: %d\n", changes_made);
         printf("\n");
         print_sudoku();
 
@@ -92,7 +99,7 @@ int main(int argc, char** argv){
         return 0;
 }
 
-static void attempt_square_insertions(int square_x, int square_y){
+static int attempt_square_insertions(int square_x, int square_y){
         int** square;
         struct array* missing_numbers;
         int i;
@@ -104,12 +111,15 @@ static void attempt_square_insertions(int square_x, int square_y){
         int option_col;
         int* row_array;
         int* col_array;
+        int changes_made;
 
         square = get_square(square_x, square_y);
         missing_numbers = calculateSquareMissingNumbers(square);
 
         print_3x3_array(square);
         print_array_struct(missing_numbers);
+
+        changes_made = 0;
 
         for(i = 0; i < missing_numbers->size; ++i){
                 num = missing_numbers->data[i];
@@ -139,14 +149,17 @@ static void attempt_square_insertions(int square_x, int square_y){
                         set_val(option_row, option_col, num);
                         printf("Change made\n");
                         print_sudoku();
+                        ++changes_made;
                 }
         }
 
         free_2d_array(square);
         free_array_struct(missing_numbers);
+
+        return changes_made;
 }
 
-static void attempt_col_insertions(int col_num){
+static int attempt_col_insertions(int col_num){
         int* col;
         struct array* missing_numbers;
         int i;
@@ -158,6 +171,7 @@ static void attempt_col_insertions(int col_num){
         int option_row;
         int* row_array;
         int** square_array;
+        int changes_made;
 
         square_y = col_num / 3;
 
@@ -167,6 +181,8 @@ static void attempt_col_insertions(int col_num){
 
         print_size9_array(col);
         print_array_struct(missing_numbers);
+
+        changes_made = 0;
 
         for(i = 0; i < missing_numbers->size; ++i){
                 num = missing_numbers->data[i];
@@ -194,15 +210,17 @@ static void attempt_col_insertions(int col_num){
                         set_val(option_row, col_num, num);
                         printf("Change made\n");
                         print_sudoku();
+                        ++changes_made;
                 }
         }
 
         free_1d_array(col);
         free_array_struct(missing_numbers);
 
+        return changes_made;
 }
 
-static void attempt_row_insertions(int row_num){
+static int attempt_row_insertions(int row_num){
         int* row;
         struct array* missing_numbers;
         int i;
@@ -214,6 +232,7 @@ static void attempt_row_insertions(int row_num){
         int option_col;
         int* col_array;
         int** square_array;
+        int changes_made;
 
         square_x = row_num / 3;
 
@@ -223,6 +242,8 @@ static void attempt_row_insertions(int row_num){
 
         //print_size9_array(row);
         //print_array_struct(missing_numbers);
+
+        changes_made = 0;
 
         for(i = 0; i < missing_numbers->size; ++i){
                 num = missing_numbers->data[i];
@@ -250,9 +271,12 @@ static void attempt_row_insertions(int row_num){
                         set_val(row_num, option_col, num);
                         printf("Change made\n");
                         print_sudoku();
+                        ++changes_made;
                 }
         }
 
         free_1d_array(row);
         free_array_struct(missing_numbers);
+
+        return changes_made;
 }
